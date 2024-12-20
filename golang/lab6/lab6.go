1aab4ec66847ee0c9e20cb946dca5e7689c16ad9
HeX04yXa123 <boin111000@gmail.com>
2024-12-20 17:02:25 +0300

    lab6

---
+++ golang/lab6/lab6.go
@@ -0,0 +1,35 @@
+package lab6
+
+import "fmt"
+
+type Telephone struct {
+	Model        string
+	Weight       float64
+	display_size float64
+}
+
+func NewTelephone(Model string, Weight, display_size float64) Telephone {
+	return Telephone{
+		Model:        Model,
+		Weight:       Weight,
+		display_size: display_size,
+	}
+
+}
+
+func (f Telephone) Info() string {
+	return fmt.Sprintf("Модель: %s, Вес модели: %s, Размер дисплея модели: %d минут", f.Model, f.Weight, f.display_size)
+}
+func (f *Telephone) UpdateModel(newModel string) {
+	f.Model = newModel
+}
+func (f Telephone) Buy() {
+	fmt.Printf("Вы купили телефон модели:%s веса:%s у которого размер дисплея:%s", f.Model, f.Weight, f.display_size)
+}
+func ExecuteLab6() {
+	Telephone := NewTelephone("Iphone 15 Pro Max", 0.221, 6.7)
+	Telephone.Buy()
+	fmt.Println(Telephone.Info())
+	Telephone.UpdateModel("Iphone 16")
+	fmt.Println(Telephone.Info())
+}
